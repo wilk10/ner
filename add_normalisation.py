1406e4b75d4723860f8489f29b7a8cd27ad668ac
Anselmo Sampietro <ans.samp@gmail.com>
2019-08-26 11:54:27 +0200

    normalisation for animals with catalogue of life

--- add_normalisation.py
+++ add_normalisation.py
@@ -1,4 +1,3 @@
-import re
 from spacy_deep_learning import SpacyDeepLearning
 from utils.data import Data
 from utils.eppo import Eppo
@@ -7,6 +6,10 @@ from utils.cat_life import CatLife
 
 class Normalisation:
     NOMENCLATURE_BY_KINGDOM = {'plant': 'EPPO Global Database', 'animal': 'Catalogue of Life'}
+    ANIMAL_CONVERSIONS = {
+        'domestic cat': ['cat', 'feline'], 'domestic dog': ['dog', 'canine'], 'human': ['patient', 'person'],
+        'horse': ['equine'], 'domestic goat': ['goat'], 'sus scrofa': ['pig'], 'ovis aries': ['sheep']}
+    ACCEPTED_LANGUAGES = ['english', 'latin', 'en', 'la']
 
     def __init__(self):
         self.data = Data()
@@ -36,6 +39,21 @@ class Normalisation:
                     full_version_entity = ' '.join([previous_word, entity_words[-1]])
         return full_version_entity
 
+    def get_possible_animal_entities(self, entity, text):
+        entity = entity.strip().lower()
+        without_s = entity[:-1] if entity[-1] == 's' else entity
+        final_entities = []
+        for ent in {entity, without_s}:
+            alternative_names = [name for name, inputs in self.ANIMAL_CONVERSIONS.items() if ent in inputs]
+            if alternative_names:
+                final_entities.extend(alternative_names)
+            else:
+                final_entities.append(ent)
+                full_entity = self.find_matching_full_version_entity(text, ent)
+                if full_entity is not None:
+                    final_entities.append(full_entity)
+        return final_entities
+
     def get_concept_id(self, entity, bioconcept, text):
         if bioconcept == 'PLANT_SPECIES':
             concept_id = self.get_eppo_code(entity)
@@ -49,22 +67,38 @@ class Normalisation:
             print(f'ENTITY: {entity}. conceptID: {concept_id}')
         else:
             assert bioconcept == 'TARGET_SPECIES'
-            response = self.cat_life.get_response(entity)
-            # if any result, need to look for the ones that have a name that equals the entity.
-            #   having the entity contained in the name is not enough
-            # another check is "source_database" == "ITIS Global: The Integrated Taxonomic Information System"
-            #   or at least give more weight to those results
-            # the look for the ID in either the result itself, or inside "accepted_name" dictionary
-            import pdb
-            pdb.set_trace()
+            print(f'\nsearching for: {entity.upper()}')
             concept_id = None
+            possible_entities = self.get_possible_animal_entities(entity, text)
+            id_found = False
+            for possible_entity in possible_entities:
+                response = self.cat_life.get_response(possible_entity)
+                if 'results' in response.keys():
+                    for result in response['results']:
+                        if possible_entity == result['name'].lower():
+                            if result['name_status'] == 'accepted name':
+                                if 'id' in result.keys():
+                                    concept_id = result['id']
+                                    id_found = True
+                            elif 'language' in result.keys() and 'accepted_name' in result.keys():
+                                if result['language'].lower() in self.ACCEPTED_LANGUAGES:
+                                    if 'id' in result['accepted_name']:
+                                        concept_id = result['accepted_name']['id']
+                                        id_found = True
+                            if id_found:
+                                print(f'!!! EXACT name match for {possible_entity}. ID is {concept_id}')
+                                break
+                if id_found:
+                    break
+            if not id_found:
+                print(f'no match..? {entity}')
         return concept_id
 
     def run(self):
         n_items_by_bioconcept = {'LOCATION': 20000, 'YEAR': 7000}
         spacy_deep_learning = SpacyDeepLearning(n_items_by_bioconcept)
         model_dir_by_bioconcept = spacy_deep_learning.train_models_or_get_dirs()
-        for kingdom in ['plant', 'animal']:
+        for kingdom in ['animal', 'plant']:
             validation_data = self.data.read_json(kingdom, 'validation')
             kingdom_classifications = []
             for i, item in enumerate(validation_data['result']):
@@ -84,7 +118,6 @@ class Normalisation:
                             'Nomenclature': self.NOMENCLATURE_BY_KINGDOM[kingdom],
                             'conceptID': concept_id}
                         item_classifications.append(classification)
-                input()
                 kingdom_classifications.append(item_classifications)
             print(kingdom_classifications)
 
